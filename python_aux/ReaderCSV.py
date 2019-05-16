30a18fa48090e0a3c6d6c0ff1344f859e46098f4
Lucas Seara Manoel <ls.manoel@gmail.com>
2019-05-16 10:43:39 -0300

    update data_list to stack_axes_list

--- python_aux/ReaderCSV.py
+++ python_aux/ReaderCSV.py
@@ -30,23 +30,49 @@ class DataList:
 			# ==================================================
 			# -- > Estrutura data_list
 			#
-			#	data_list [index]{'info', 'data'}
+			#	stack_axes_list [index]{}
 			#		'info'		: info_dict 
 			#		'axes'		: axes_dict
-			#	
-			#		axes_dict{'time', 'freq'}
+			#		
+			#		info_dict{...}
+	        #    		'Record Length'		: record_length
+	        #    		'Sample Interval'	: sample_interval
+	        #    		'Source'			: source
+	        #    		'Vertical Units'	: vertical_units
+	        #    		'Vertical Scale'	: vertical_scale
+	        #    		'Vertical Offset'	: vertical_offset
+	        #    		'Horizontal Units'	: horizontal_units
+	        #    		'Horizontal Scale'	: horizontal_scale
+	        #    		'Pt Fmt'			: pt_fmt
+	        #    		'Yzero'				: yzero
+	        #    		'Probe Atten'		: probe_atten
+	        #    		'Model Number'		: model_number
+	        #    		'Serial Number'		: serial_number
+	        #    		'Firmware Version'	: firmware_version
+			#
+			#		axes_dict{}
 			#			'time'		: time_dict						
 			#			'freq'		: freq_dict					 	
+			#			
+			#			time_dict{}
+			#				't'			: data_dict  
+			#				'x'			: data_dict 
+			#
+			#			freq_dict{}
+			#				'f'			: data_dict 	  	 		 		    
+			#				'H'			: data_dict 			 		 
+			#				'H_dB'		: data_dict 				
 			#		
-			#			time_dict{'t', 'x'}
-			#				't'			: [np.array]  
-			#				'x'			: [np.array]		 		    
-			#		
-			#			freq_dict{'f', 'H', 'H_db'}
-			#				'f'			: [np.array] 	  	 		 		    
-			#				'H'			: [np.array] 			 		 
-			#				'H_dB'		: [np.array] 				
-			#		
+			#				data_dict{}
+			#					'array'			: [np.array] 
+			#					'config_plot'	: config_plot_dict
+			#					
+			#					config_plot_dict{}
+			#						'legend'		: string
+			#						'title'			: string
+			#						'xlabel'		: string
+			#						'ylabel'		: string
+			#					
 			self._data_list = self.file2data_list(self._file_list)
 			# =====================================================
 
